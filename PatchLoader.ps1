55da36b79c7c5f9ed042811277230fd727e4bd79
Vadim <rudometov.vadim123@gmail.com>
2026-09-02 20:43:58 +0300

    fix PatchLoader.ps1

--- PatchLoader.ps1
+++ PatchLoader.ps1
@@ -9,6 +9,8 @@ enum _Coh2State {
 }
 
 $PATH = "."
+Set-Location $PATH
+
 $PATCH = @'
     $COH2PATH = "{0}"
     $langs = Get-WinUserLanguageList
@@ -31,54 +33,51 @@ $PATCH = @'
     }
 '@
 
-function _GetCoh2State([Parameter(Mandatory)] [string]$path) {
-    return $(if (Test-Path -Path "$path\__reliccoh2.exe") { [_Coh2State]::Patched }
-            elseif (Test-Path -Path "$path\reliccoh2.exe") { [_Coh2State]::Yes }
+function _GetCoh2State {
+    return $(if (Test-Path -Path "__reliccoh2.exe") { [_Coh2State]::Patched }
+            elseif (Test-Path -Path "reliccoh2.exe") { [_Coh2State]::Yes }
             else { [_Coh2State]::No })
 }
-function _CreatePatch([Parameter(Mandatory)] [string]$coh2exe,
-                      [Parameter(Mandatory)] [string]$outfile) {
-    Invoke-ps2exe -InputFile $PATCH.Replace("{0}", $coh2exe) -OutputFile $outfile -Verbose -NoConsole
+function _TestCoh2State() {
+    $coh2state = _GetCoh2State $PATH
+    if ($coh2state -eq ([_Coh2State]::No))
+    {
+        Write-Error "No COH2 found"
+        throw
+    }
+    elseif ($coh2state -eq ([_Coh2State]::Patched)) 
+    {
+        Write-Output "Deleting previous patch"
+        try {
+            Remove-Item -Path "reliccoh2.exe"
+            Rename-Item -Path "__reliccoh2.exe" -NewName "RelicCoH2.exe"
+        }
+        catch { 
+            Write-Warning "Couldn't delete patch, proceeding anyway"
+        }
+    }
 }
 function _InstallPatch {
-    if (-not (Get-Command Invoke-PS2EXE -ErrorAction SilentlyContinue))
+    try { Get-Command Invoke-PS2EXE }
+    catch
     {
         Install-Module -Name ps2exe -RequiredVersion 1.0.18 -Scope CurrentUser -ErrorAction Stop
     }
 
-    Rename-Item "$PATH\RelicCoH2.exe" -NewName "__RelicCoH2.exe"
-    _CreatePatch -coh2exe "__RelicCoH2.exe" -outfile "$PATH\RelicCoH2.exe"
+    Rename-Item "RelicCoH2.exe" -NewName "__RelicCoH2.exe"
+    Invoke-ps2exe -InputFile "patch.ps1" -OutputFile "$PATH\RelicCoH2.exe" -Verbose -NoConsole
     Write-Output "Actual COH2 executable renamed to __RelicCoH2.exe"
 }
 
-if ($extract)
-{
-    Write-Output "Patch extracted to patch.ps1"
-    Write-Output $PATCH | Out-File patch.ps1
-    return
-}
-
-$coh2state = _GetCoh2State $PATH
-if ($coh2state -eq ([_Coh2State]::No))
-{
-    Write-Error "No COH2 found"
-    return
-}
-elseif ($coh2state -eq ([_Coh2State]::Patched)) 
-{
-    Write-Output "Deleting previous patch"
-    try {
-        Remove-Item -Path "$PATH\reliccoh2.exe"
-        Rename-Item -Path "$PATH\__reliccoh2.exe" -NewName "RelicCoH2.exe"
-    }
-    catch { 
-        Write-Warning "Couldn't delete patch, proceeding anyway"
-    }
-}
 
 try
 {
-    _InstallPatch
+    $contents = $(if ($extract) { $PATCH } else { $PATCH.Replace("{0}", "__RelicCoH2.exe") })
+    Write-Output $contents | Out-File patch.ps1
+    if (-not $extract) {
+        _TestCoh2State
+        _InstallPatch
+    }
 }
 catch
 {
